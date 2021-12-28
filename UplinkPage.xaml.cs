2e9158717e2af59d967d10b7034ef7c8e47b1dc0
Jordan <JordanR5a@users.noreply.github.com>
2021-12-28 12:17:10 -0700

    MainPage, Uplink basics

--- UplinkPage.xaml.cs
+++ UplinkPage.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using TypingTrainer.DatabaseObjects;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -13,18 +16,79 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace TypingTrainer
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class UplinkPage : Page
     {
         public UplinkPage()
         {
             this.InitializeComponent();
+        }
+
+        private string SendGetRequest()
+        {
+            //Create an HTTP client object
+            Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
+
+            //Add a user-agent header to the GET request. 
+            var headers = httpClient.DefaultRequestHeaders;
+
+            //The safe way to add a header value is to use the TryParseAdd method and verify the return value is true,
+            //especially if the header value is coming from user input.
+            string header = "ie";
+            if (!headers.UserAgent.TryParseAdd(header))
+            {
+                throw new Exception("Invalid header value: " + header);
+            }
+
+            header = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)";
+            if (!headers.UserAgent.TryParseAdd(header))
+            {
+                throw new Exception("Invalid header value: " + header);
+            }
+
+            Uri requestUri = new Uri("http://localhost:8080/book");
+
+            //Send the GET request asynchronously and retrieve the response as a string.
+            Windows.Web.Http.HttpResponseMessage httpResponse = new Windows.Web.Http.HttpResponseMessage();
+            string httpResponseBody = "";
+
+            try
+            {
+                //Send the GET request
+                httpResponse = httpClient.GetAsync(requestUri).AsTask().GetAwaiter().GetResult();
+                httpResponse.EnsureSuccessStatusCode();
+                httpResponseBody = httpResponse.Content.ReadAsStringAsync().AsTask().GetAwaiter().GetResult();
+            }
+            catch (Exception ex)
+            {
+                httpResponseBody = "Error: " + ex.HResult.ToString("X") + " Message: " + ex.Message;
+            }
+
+            return httpResponseBody;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+
+            List<Book> books = JsonConvert.DeserializeObject<List<Book>>(SendGetRequest());
+            CreateBookDisplay(books);
+
+        }
+
+        private void CreateBookDisplay(List<Book> books)
+        {
+            foreach (Book book in books)
+            {
+                Button btn = new Button();
+                btn.Name = book.BookID.ToString();
+                btn.HorizontalAlignment = HorizontalAlignment.Stretch;
+                btn.Content = book.BookTitle;
+                btn.FontSize = 40;
+                btn.Margin = new Thickness(10);
+                btn.CornerRadius = new CornerRadius(25);
+                UplinkPageBookDisplay.Children.Add(btn);
+            }
         }
     }
 }
